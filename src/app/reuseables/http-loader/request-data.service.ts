23309b631e071f548615b21b92bb7dc0c159ecb9
Daniel <onyemordidaniel@gmail.com>
2026-04-20 10:05:04 +0100

    all

--- src/app/reuseables/http-loader/request-data.service.ts
+++ src/app/reuseables/http-loader/request-data.service.ts
@@ -4,8 +4,8 @@ import { finalize, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RequestDataService {
-  private baseUrl = 'http://127.0.0.1:8000/api'; // Replace with your API endpoint
-  // private baseUrl = "/api";
+  // private baseUrl = 'http://127.0.0.1:8000/api'; // Replace with your API endpoint
+  private baseUrl = "/api";
 
 // dfg
   constructor(
